df8cc8d809bf2a19ba2a9c82a8316760cc769669
Mathews Alufwani <mathewalufwani@gmail.com>
2020-07-21 06:35:13 +0300

    edit static files

--- config.py
+++ config.py
@@ -3,26 +3,54 @@ class Config:
     '''
     General configuration parent class
     '''
+    QUOTE_API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://alufwany:alufwany@localhost/thefamousblog'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+    @staticmethod
+    def init_app(app):
+        pass
 
 
 
 class ProdConfig(Config):
     '''
-    Production configuration child class
+    Production  configuration child class
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://alufwany:alufwany@localhost/tfb_test'
+
 
 class DevConfig(Config):
     '''
-    Production configuration child class
+    Development  configuration child class
     Args:
         Config: The parent configuration class with General configuration settings
     '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://alufwany:alufwany@localhost/thefamousblog'
+
     DEBUG = True
 
 config_options = {
-'development':DevConfig,
-'production':ProdConfig
-}
+    'development': DevConfig,
+    'production': ProdConfig,
+    'test': TestConfig
+}    
