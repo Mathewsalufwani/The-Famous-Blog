4fa08258d4cb8a999be8423864f4e69e05a70972
Mathews Alufwani <mathewalufwani@gmail.com>
2020-07-21 06:09:34 +0300

    create blog test

---
+++ tests/test_blog.py
@@ -0,0 +1,23 @@
+from app.models import Comment, User,Blog
+from app import db
+import unittest
+
+class BlogTest(unittest.TestCase):
+    def setUp(self):
+        self.new_user = User(username = 'Minions', password = 'matata', email = 'janedoe@demo.com')
+        self.new_blog = Blog(blog_author='Blog', blog_content='Blog content')
+        
+    def test_check_instance(self):
+        
+        self.assertEquals(self.new_blog.blog_author,'Blog')
+        self.assertEquals(self.new_blog.blog_content, 'Blog content')
+        
+
+    def test_save_blog(self):
+        self.new_blog.save_blog()
+        self.assertTrue(len(Blog.query.all()) > 0)      
+
+    def test_get_blog_by_id(self):
+        self.new_blog.save_blog()
+        blog = Blog.get_blog(1)
+        self.assertTrue(blog is not None)
