982f847d0f746d33347014f44a6a08e4eed742cb
Mathews Alufwani <mathewalufwani@gmail.com>
2020-07-21 04:19:20 +0300

    add random quote api

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,8 @@
 from flask import render_template,request,redirect,url_for
 from . import main
+from ..requests import get_quote
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    quote = get_quote()
+    return render_template('index.html',quote=quote)
